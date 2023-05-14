f0c87f69e30528e36fc3f03b55424b158c216c7d
Kaustuv Karki <karkikaustuv@gmail.com>
2023-05-15 00:08:44 +0545

    ramdom suggestion made

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -23,13 +23,21 @@ const Popular = () => {
     return (
         <Wrapper>
             <h3>Popular Items</h3>
-            <Splide>
+            <Splide
+                options={{
+                    perPage: 4,
+                    arrows: false,
+                    pagination: false,
+                    drag: "free",
+                    gap: "5rem",
+                }}>
                 {popular.map((recipe) => {
                     return (
                         <SplideSlide>
                             <Card>
                                 <p>{recipe.title}</p>
                                 <img src={recipe.image} alt={recipe.title} />
+                                <Gradient />
                             </Card>
                         </SplideSlide>
                     );
@@ -45,12 +53,42 @@ const Wrapper = styled.div`
 
 const Card = styled.div`
     min-height: 25rem;
-    border-radius: 2rem;
+    border-radius: 1rem;
     overflow: hidden;
+    position: relative;
 
     img {
         border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
     }
+
+    p {
+        position: absolute;
+        z-index: 10;
+        left: 55%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        width: 100%;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+const Gradient = styled.div`
+    z-index: 3;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
 export default Popular;
